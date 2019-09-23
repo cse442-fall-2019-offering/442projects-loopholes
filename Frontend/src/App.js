b999477ae91a5e6420283679be5295a5ec3366df
edentron <cleverforesttrees@gmail.com>
2019-09-23 15:43:44 -0400

    Basic search bar structure
    Created searchbar.js in components. Preliminary code for search bar, won't show up on localhost

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './components/searchbar.js'
+import SearchBar from "./components/searchbar.js"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-        <p>
-          UBulletin
-        </p>
-        <button className="button-prop">
-
-        </button>
-
-        <button className="button-prop">
-
-        </button>
-
-      </header>
-    </div>
-  );
+      <div>
+        <SearchBar/>
+      </div>
+  )
 }
 
 export default App;
+
