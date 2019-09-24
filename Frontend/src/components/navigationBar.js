d7de981d85b2b7204ee00f5526d525af06596d39
edentron <cleverforesttrees@gmail.com>
2019-09-23 23:39:35 -0400

    Created Sort dropdown menu #33
    added the sort dropdown menu to the navigation bar

--- Frontend/src/components/navigationBar.js
+++ Frontend/src/components/navigationBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
 import Form from 'react-bootstrap/Form'
 import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
@@ -20,6 +21,9 @@ class navigationBar extends React.Component {
                     <Nav className="mr-auto">
                         <Nav.Link className="NavigationBarLink" href="#home">Home</Nav.Link>
                         <Nav.Link className="NavigationBarLink" href="#create">Create</Nav.Link>
+                        <NavDropdown title="Sort" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Date</NavDropdown.Item>
+                        </NavDropdown>
                     </Nav>
                 <Form inline>
                     <FormControl type="text" placeholder="Search" className="mr-sm-2" />
