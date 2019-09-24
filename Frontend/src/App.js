72ef8b914782456603a96b78ca6cef6469c5d3c6
edentron <cleverforesttrees@gmail.com>
2019-09-23 23:06:19 -0400

    Prepping for merge
    Committing local changes in App.js and index.css

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
-//import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './components/searchbar.js'
-import SearchBar from "./components/searchbar.js"
+import './App.css';
+import NavigationBar from "./components/navigationBar.js";
 
 function App() {
-  return (
-      <div>
-        <SearchBar/>
-      </div>
-  )
-}
-
+    return (
+        <div>
+          <NavigationBar/>
+        </div>
+    )
+  }
 export default App;
-
