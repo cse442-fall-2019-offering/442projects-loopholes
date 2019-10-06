06fe31f35132123cd220dfcee6ef4d4e3539d2d6
Supratik Neupane <sneupane@buffalo.edu>
2019-10-06 19:27:56 -0400

    Added initial test for sorting newest to oldest posts #32
    SortingTest.ts currently tests for newest to oldest post sorting

---
+++ backend/src/querying/SortingTest.ts
@@ -0,0 +1,10 @@
+import "mocha";
+import { expect } from "chai";
+import { PostMetadata } from "../objects
+
+describe("Newest", () => {
+  it("newest post should come first", () =>
+    const posts: PostMetadata[] {};
+  );
+  expect(posts[0].id == 1);
+});
