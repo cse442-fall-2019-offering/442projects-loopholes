f9c94206557323b76c751db3d6b65b9fdd0d4e28
Hans Bas <haanmiba@gmail.com>
2019-10-19 17:39:22 -0400

    Fixed homepage card rendering * Created `homepageCard.js` for rendering a card per image - stateless functional component * Changed `homepage.js` to render only cards for images fetched from backend

--- Frontend/src/components/homepage.js
+++ Frontend/src/components/homepage.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 import CardColumns from 'react-bootstrap/CardColumns';
+import HomepageCard from './homepageCard';
 import * as Endpoint from '../constants/Endpoint';
 import ImageUploader from '../firebase/ImageUploader';
 import { getTimestampImageString } from '../util/helper-functions/timestamp';
@@ -36,159 +37,16 @@ class homePage extends React.Component {
 
     render(){
 
-      let images = this.state.imageLinks.map(link => <img src={link} />)
+      let images = this.state.imageLinks.map(link => <img src={link} />);
+      let cards = this.state.imageLinks.map(link => <HomepageCard imageLink={link} title="Untitled" date="N/A" time="N/A" place="N/A" />);
 
         return(
 <div>
   {this.state.message}
 <br></br>
 <input type="file" onChange={this.uploadFile} accept="image/*"></input>
-{images}
 <CardColumns>
-  <Card border="primary" bg="light" text="dark">
-    <Card.Img variant="top" src="https://calendarmedia.blob.core.windows.net/assets/cfcab72d-b7b1-442d-bd2d-25097794861c.jpg" />
-    <Card.Body>
-      <Card.Title>International Tea Time
-            <br></br>
-            <br></br>
-            <small>
-                <div className="font-weight-bold">Date:</div> September 25th
-                <br></br>
-                <br></br>
-                <div className="font-weight-bold">Time:</div> 5:00PM EDT
-                <br></br>
-                <br></br>
-                <div className="font-weight-bold">Place:</div> 240 Student Union, North Campus, UB
-            </small>
-      </Card.Title>
-    </Card.Body>
-    <Card.Footer>
-            <div className="text-right">
-                <Form.Check text="dark" type="checkbox" label="Like" />
-            </div>
-        
-    </Card.Footer>
-  </Card>
-<Card border="primary" bg="light" text="dark">
-    <Card.Img variant="top" src="https://calendarmedia.blob.core.windows.net/assets/cfcab72d-b7b1-442d-bd2d-25097794861c.jpg" />
-    <Card.Body>
-      <Card.Title>International Tea Time
-            <br></br>
-            <br></br>
-            <small>
-                <div className="font-weight-bold">Date:</div> October 2nd
-                <br></br>
-                <br></br>
-                <div className="font-weight-bold">Time:</div> 5:00PM EDT
-                <br></br>
-                <br></br>
-                <div className="font-weight-bold">Place:</div> 240 Student Union, North Campus, UB
-            </small>
-      </Card.Title>
-    </Card.Body>
-    <Card.Footer>
-            <div className="text-right">
-                <Form.Check text="dark" type="checkbox" label="Like" />
-            </div>
-        
-    </Card.Footer>
-  </Card>
-  <Card border="primary" bg="light" text="dark">
-    <Card.Img variant="top" src="https://calendarmedia.blob.core.windows.net/assets/cfcab72d-b7b1-442d-bd2d-25097794861c.jpg" />
-    <Card.Body>
-      <Card.Title>International Tea Time
-            <br></br>
-            <br></br>
-            <small>
-                <div className="font-weight-bold">Date:</div> October 9th
-                <br></br>
-                <br></br>
-                <div className="font-weight-bold">Time:</div> 5:00PM EDT
-                <br></br>
-                <br></br>
-                <div className="font-weight-bold">Place:</div> 240 Student Union, North Campus, UB
-            </small>
-      </Card.Title>
-    </Card.Body>
-    <Card.Footer>
-            <div className="text-right">
-                <Form.Check text="dark" type="checkbox" label="Like" />
-            </div>
-        
-    </Card.Footer>
-  </Card>
-  <Card border="primary" bg="light" text="dark">
-    <Card.Img variant="top" src="https://calendarmedia.blob.core.windows.net/assets/cfcab72d-b7b1-442d-bd2d-25097794861c.jpg" />
-    <Card.Body>
-      <Card.Title>International Tea Time
-            <br></br>
-            <br></br>
-            <small>
-                <div className="font-weight-bold">Date:</div> October 16th
-                <br></br>
-                <br></br>
-                <div className="font-weight-bold">Time:</div> 5:00PM EDT
-                <br></br>
-                <br></br>
-                <div className="font-weight-bold">Place:</div> 240 Student Union, North Campus, UB
-            </small>
-      </Card.Title>
-    </Card.Body>
-    <Card.Footer>
-            <div className="text-right">
-                <Form.Check text="dark" type="checkbox" label="Like" />
-            </div>
-        
-    </Card.Footer>
-  </Card>
-  <Card border="primary" bg="light" text="dark">
-    <Card.Img variant="top" src="https://calendarmedia.blob.core.windows.net/assets/cfcab72d-b7b1-442d-bd2d-25097794861c.jpg" />
-    <Card.Body>
-      <Card.Title>International Tea Time
-            <br></br>
-            <br></br>
-            <small>
-                <div className="font-weight-bold">Date:</div> October 23rd
-                <br></br>
-                <br></br>
-                <div className="font-weight-bold">Time:</div> 5:00PM EDT
-                <br></br>
-                <br></br>
-                <div className="font-weight-bold">Place:</div> 240 Student Union, North Campus, UB
-            </small>
-      </Card.Title>
-    </Card.Body>
-    <Card.Footer>
-            <div className="text-right">
-                <Form.Check text="dark" type="checkbox" label="Like" />
-            </div>
-        
-    </Card.Footer>
-  </Card>
-  <Card border="primary" bg="light" text="dark">
-    <Card.Img variant="top" src="https://calendarmedia.blob.core.windows.net/assets/cfcab72d-b7b1-442d-bd2d-25097794861c.jpg" />
-    <Card.Body>
-      <Card.Title>International Tea Time
-            <br></br>
-            <br></br>
-            <small>
-                <div className="font-weight-bold">Date:</div> October 30th
-                <br></br>
-                <br></br>
-                <div className="font-weight-bold">Time:</div> 5:00PM EDT
-                <br></br>
-                <br></br>
-                <div className="font-weight-bold">Place:</div> 240 Student Union, North Campus, UB
-            </small>
-      </Card.Title>
-    </Card.Body>
-    <Card.Footer>
-            <div className="text-right">
-                <Form.Check text="dark" type="checkbox" label="Like" />
-            </div>
-        
-    </Card.Footer>
-  </Card>
+  {cards}
 </CardColumns>
 </div>
         );
