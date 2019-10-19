16b91ca1dc4bff37000dc99fda93886dc54ad711
edentron <cleverforesttrees@gmail.com>
2019-10-19 14:55:49 -0400

    Streamlined zoom structure and trimmed example posts #50 #23
    Added the basic outline of how zoom will function. Removed all but one example post. The current example post should show how redirect will work. Planning on adding redirect to branch for homepage instead

--- Frontend/src/components/homepage.js
+++ Frontend/src/components/homepage.js
@@ -4,6 +4,8 @@ import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 import CardColumns from 'react-bootstrap/CardColumns'
 import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
+import ZoomIn from './zoomin.js';
 
 class homePage extends React.Component {
     constructor(props) {
@@ -18,7 +20,13 @@ class homePage extends React.Component {
 <br></br>
 <CardColumns>
   <Card border="primary" bg="light" text="dark">
-    <Card.Img variant="top" src="https://calendarmedia.blob.core.windows.net/assets/cfcab72d-b7b1-442d-bd2d-25097794861c.jpg" />
+    <Nav>
+        <Nav.Item>
+                <Nav.Link href="https://calendarmedia.blob.core.windows.net/assets/cfcab72d-b7b1-442d-bd2d-25097794861c.jpg" target="_blank">
+                        <Card.Img variant="top" src="https://calendarmedia.blob.core.windows.net/assets/cfcab72d-b7b1-442d-bd2d-25097794861c.jpg"/>
+                    </Nav.Link>
+                </Nav.Item>
+    </Nav>
     <Card.Body>
       <Card.Title>International Tea Time
             <br></br>
@@ -33,6 +41,7 @@ class homePage extends React.Component {
                 <div className="font-weight-bold">Place:</div> 240 Student Union, North Campus, UB
             </small>
       </Card.Title>
+            <ZoomIn></ZoomIn>
     </Card.Body>
     <Card.Footer>
             <div className="text-right">
@@ -41,129 +50,9 @@ class homePage extends React.Component {
         
     </Card.Footer>
   </Card>
-<Card border="primary" bg="light" text="dark">
-    <Card.Img variant="top" src="https://calendarmedia.blob.core.windows.net/assets/cfcab72d-b7b1-442d-bd2d-25097794861c.jpg" />
-    <Card.Body>
-      <Card.Title>International Tea Time
-            <br></br>
-            <br></br>
-            <small>
-                <div className="font-weight-bold">Date:</div> October 2nd
-                <br></br>
-                <br></br>
-                <div className="font-weight-bold">Time:</div> 5:00PM EDT
-                <br></br>
-                <br></br>
-                <div className="font-weight-bold">Place:</div> 240 Student Union, North Campus, UB
-            </small>
-      </Card.Title>
-    </Card.Body>
-    <Card.Footer>
-            <div className="text-right">
-                <Form.Check text="dark" type="checkbox" label="Like" />
+            </CardColumns>
             </div>
-        
-    </Card.Footer>
-  </Card>
-  <Card border="primary" bg="light" text="dark">
-    <Card.Img variant="top" src="https://calendarmedia.blob.core.windows.net/assets/cfcab72d-b7b1-442d-bd2d-25097794861c.jpg" />
-    <Card.Body>
-      <Card.Title>International Tea Time
-            <br></br>
-            <br></br>
-            <small>
-                <div className="font-weight-bold">Date:</div> October 9th
-                <br></br>
-                <br></br>
-                <div className="font-weight-bold">Time:</div> 5:00PM EDT
-                <br></br>
-                <br></br>
-                <div className="font-weight-bold">Place:</div> 240 Student Union, North Campus, UB
-            </small>
-      </Card.Title>
-    </Card.Body>
-    <Card.Footer>
-            <div className="text-right">
-                <Form.Check text="dark" type="checkbox" label="Like" />
-            </div>
-        
-    </Card.Footer>
-  </Card>
-  <Card border="primary" bg="light" text="dark">
-    <Card.Img variant="top" src="https://calendarmedia.blob.core.windows.net/assets/cfcab72d-b7b1-442d-bd2d-25097794861c.jpg" />
-    <Card.Body>
-      <Card.Title>International Tea Time
-            <br></br>
-            <br></br>
-            <small>
-                <div className="font-weight-bold">Date:</div> October 16th
-                <br></br>
-                <br></br>
-                <div className="font-weight-bold">Time:</div> 5:00PM EDT
-                <br></br>
-                <br></br>
-                <div className="font-weight-bold">Place:</div> 240 Student Union, North Campus, UB
-            </small>
-      </Card.Title>
-    </Card.Body>
-    <Card.Footer>
-            <div className="text-right">
-                <Form.Check text="dark" type="checkbox" label="Like" />
-            </div>
-        
-    </Card.Footer>
-  </Card>
-  <Card border="primary" bg="light" text="dark">
-    <Card.Img variant="top" src="https://calendarmedia.blob.core.windows.net/assets/cfcab72d-b7b1-442d-bd2d-25097794861c.jpg" />
-    <Card.Body>
-      <Card.Title>International Tea Time
-            <br></br>
-            <br></br>
-            <small>
-                <div className="font-weight-bold">Date:</div> October 23rd
-                <br></br>
-                <br></br>
-                <div className="font-weight-bold">Time:</div> 5:00PM EDT
-                <br></br>
-                <br></br>
-                <div className="font-weight-bold">Place:</div> 240 Student Union, North Campus, UB
-            </small>
-      </Card.Title>
-    </Card.Body>
-    <Card.Footer>
-            <div className="text-right">
-                <Form.Check text="dark" type="checkbox" label="Like" />
-            </div>
-        
-    </Card.Footer>
-  </Card>
-  <Card border="primary" bg="light" text="dark">
-    <Card.Img variant="top" src="https://calendarmedia.blob.core.windows.net/assets/cfcab72d-b7b1-442d-bd2d-25097794861c.jpg" />
-    <Card.Body>
-      <Card.Title>International Tea Time
-            <br></br>
-            <br></br>
-            <small>
-                <div className="font-weight-bold">Date:</div> October 30th
-                <br></br>
-                <br></br>
-                <div className="font-weight-bold">Time:</div> 5:00PM EDT
-                <br></br>
-                <br></br>
-                <div className="font-weight-bold">Place:</div> 240 Student Union, North Campus, UB
-            </small>
-      </Card.Title>
-    </Card.Body>
-    <Card.Footer>
-            <div className="text-right">
-                <Form.Check text="dark" type="checkbox" label="Like" />
-            </div>
-        
-    </Card.Footer>
-  </Card>
-</CardColumns>
-</div>
-        );
-    }
+            );
+}
 }
 export default homePage;
--- Frontend/src/components/zoomin.js
+++ Frontend/src/components/zoomin.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import CardColumns from 'react-bootstrap/CardColumns';
 import Image from 'react-bootstrap/Image';
-import Nav from 'react-bootstrap/Nav';
 
 class zoomIn extends React.Component {
     constructor(props) {
@@ -13,41 +9,20 @@ class zoomIn extends React.Component {
         this.state = {
             page: this.props.page
         }
+        this.handleZoom = this.handleZoom.bind(this);
+    }
+    handleZoom(){
+        
     }
     render(){
         return(
             <div>
-            <CardColumns>
-            <Card border="primary" bg="light" text="dark">
-            <Nav>
-                <Nav.Item>
-                    <Nav.Link href="https://calendarmedia.blob.core.windows.net/assets/cfcab72d-b7b1-442d-bd2d-25097794861c.jpg" target="_blank"><Card.Img variant="top" src="https://calendarmedia.blob.core.windows.net/assets/cfcab72d-b7b1-442d-bd2d-25097794861c.jpg"/></Nav.Link>
-                </Nav.Item>
-            </Nav>
-    <Card.Body>
-      <Card.Title>International Tea Time
-            <br></br>
-            <br></br>
-            <small>
-                <div className="font-weight-bold">Date:</div> September 25th
                 <br></br>
-                <br></br>
-                <div className="font-weight-bold">Time:</div> 5:00PM EDT
-                <br></br>
-                <br></br>
-                <div className="font-weight-bold">Place:</div> 240 Student Union, North Campus, UB
-            </small>
-      </Card.Title>
-    </Card.Body>
-    <Card.Footer>
-            <div className="text-right">
-                <Form.Check text="dark" type="checkbox" label="Like" />
+                <Button 
+                variant="primary" disabled onClick={this.handleZoom}>
+                    Zoom
+                </Button>
             </div>
-        
-    </Card.Footer>
-  </Card>
-</CardColumns>
-    </div>
         );
     }
 }
