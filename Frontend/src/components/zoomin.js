264050a6bd60bff94ac2f558da756a83cee9ca0e
Kai Bustos <bustoskai@gmail.com>
2019-10-21 19:06:25 -0400

    Added zoom button to homepageCard

--- Frontend/src/components/zoomin.js
+++ Frontend/src/components/zoomin.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
 import {Button, Card, Image, Modal} from 'react-bootstrap';
-import HomepageCard from 'homepageCard.js'
 
 class zoomIn extends React.Component {
 
@@ -9,7 +8,13 @@ class zoomIn extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            show: false
+            show: false,
+            imageLink,
+            title: "Untitled",
+            date: "N/A",
+            time: "N/A",
+            place: "N/A"
+
         }
         this.handleShow = this.handleShow.bind(this);
         this.handleClose = this.handleClose.bind(this);
@@ -34,10 +39,32 @@ class zoomIn extends React.Component {
                 </Button>
                   <Modal show={this.state.show} onHide={this.handleClose}>
                      <Modal.Header closeButton/>
-                     <HomepageCard/>
+                     <Card border="primary" bg="light" text="dark" style={{ width: '31rem' }}>
+                         <Card.Img variant="top" src={imageLink} />
+                             <Card.Body>
+                                 <Card.Title>{title}
+                                         <br></br>
+                                         <br></br>
+                                         <small>
+                                             <div className="font-weight-bold">Date:</div> {date}
+                                             <br></br>
+                                             <br></br>
+                                             <div className="font-weight-bold">Time:</div> {time}
+                                             <br></br>
+                                             <br></br>
+                                             <div className="font-weight-bold">Place:</div> {place}
+                                         </small>
+                                 </Card.Title>
+                             </Card.Body>
+                         <Card.Footer>
+                             <div className="text-right">
+                                 <Form.Check text="dark" type="checkbox" label="Like" />
+                             </div>
+                         </Card.Footer>
+                     </Card>
                   </Modal>
             </div>
         );
     }
 }
-export default zoomIn;
+export default ZoomIn;
