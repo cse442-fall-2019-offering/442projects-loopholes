b5a699d0c6ccf5434b381a1db7d36bff61d0f9b3
Hans Bas <haanmiba@gmail.com>
2019-11-03 17:53:31 -0500

    (#66) Linted uploadpage * Ran linter to format Frontend/src/components/uploadpage.js

--- Frontend/src/components/uploadpage.js
+++ Frontend/src/components/uploadpage.js
@@ -1,38 +1,35 @@
-import React from 'react';
+import React from "react";
 import ImageUploader from "../firebase/ImageUploader";
-import 'filepond/dist/filepond.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "filepond/dist/filepond.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-class UploadPage extends React.Component{
+class UploadPage extends React.Component {
+  choseFileHandler = event => {
+    this._imgToUpload = event.target.files[0];
+    var td = new Date();
+    var yr = td.getFullYear();
+    var mon = String(td.getMonth() + 1).padStart(2, "0");
+    var day = String(td.getDate()).padStart(2, "0");
+    var hr = String(td.getHours());
+    var min = String(td.getMinutes());
+    var sec = String(td.getSeconds());
+    this._nameToUpload = yr + mon + day + hr + min + sec;
+  };
 
-    choseFileHandler = event => {
-        this._imgToUpload = event.target.files[0];
-        var td = new Date();
-        var yr = td.getFullYear();
-        var mon = String(td.getMonth() + 1).padStart(2,'0');
-        var day = String(td.getDate()).padStart(2,'0');
-        var hr = String(td.getHours());
-        var min = String(td.getMinutes());
-        var sec = String(td.getSeconds());
-        this._nameToUpload = yr + mon + day + hr + min + sec;
-    };
+  startUpload = () => {
+    let sender = new ImageUploader(this._imgToUpload, this._nameToUpload);
+    sender.sendImageToFirebaseStorage();
+  };
 
-    startUpload = () => {
-        let sender = new ImageUploader(this._imgToUpload, this._nameToUpload);
-        sender.sendImageToFirebaseStorage();
-    };
-
-    render() {
-        return(
-            <div className={'page_content'}>
-                <h1> Upload a File to Make a Post </h1>
-                <input type="file" onChange={this.choseFileHandler}/>
-                <button onClick={this.startUpload}>
-                    Upload
-                </button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className={"page_content"}>
+        <h1> Upload a File to Make a Post </h1>
+        <input type="file" onChange={this.choseFileHandler} />
+        <button onClick={this.startUpload}>Upload</button>
+      </div>
+    );
+  }
 }
 
 export default UploadPage;
