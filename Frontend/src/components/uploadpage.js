bccf3fc6e153dd247c08c3cb45f72b7523d851a8
Hans Bas <haanmiba@gmail.com>
2019-11-03 19:14:05 -0500

    (#66) Added input fields to upload page * `Frontend/src/components/uploadpage.js` now has fields for filling out event title, date, time, and location

--- Frontend/src/components/uploadpage.js
+++ Frontend/src/components/uploadpage.js
@@ -4,6 +4,13 @@ import "filepond/dist/filepond.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class UploadPage extends React.Component {
+  state = {
+    eventTitle: "",
+    eventDate: "",
+    eventTime: "",
+    eventLocation: ""
+  };
+
   choseFileHandler = event => {
     this._imgToUpload = event.target.files[0];
     var td = new Date();
@@ -21,11 +28,44 @@ class UploadPage extends React.Component {
     sender.sendImageToFirebaseStorage();
   };
 
+  onTextInputChange = (key, e) => {
+    this.setState({ [key]: e.target.value });
+  };
+
   render() {
+    const { eventTitle, eventDate, eventTime, eventLocation } = this.state;
     return (
       <div className={"page_content"}>
         <h1> Upload a File to Make a Post </h1>
         <input type="file" onChange={this.choseFileHandler} />
+        <input
+          key="test"
+          type="text"
+          onChange={e => this.onTextInputChange("eventTitle", e)}
+          value={eventTitle}
+          placeholder={"Event Title"}
+        />
+        <input
+          key="test"
+          type="text"
+          onChange={e => this.onTextInputChange("eventDate", e)}
+          value={eventDate}
+          placeholder={"Event Date"}
+        />
+        <input
+          key="test"
+          type="text"
+          onChange={e => this.onTextInputChange("eventTime", e)}
+          value={eventTime}
+          placeholder={"Event Time"}
+        />
+        <input
+          key="test"
+          type="text"
+          onChange={e => this.onTextInputChange("eventLocation", e)}
+          value={eventLocation}
+          placeholder={"Event Location"}
+        />
         <button onClick={this.startUpload}>Upload</button>
       </div>
     );
