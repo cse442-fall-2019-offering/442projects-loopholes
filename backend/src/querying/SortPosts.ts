132b4455c8e883e54dcd2414a466afde0b0b4c12
Supratik Neupane <sneupane@buffalo.edu>
2019-11-11 22:33:05 -0500

    Implemented sorting by epoch timestamp for upload date #32

---
+++ backend/src/querying/SortPosts.ts
@@ -0,0 +1,27 @@
+import { PostMetadata } from "../objects/PostMetadata";
+/**
+ * Sorts the posts based on different criteria.
+ */
+export default class SortPosts {
+
+  /**
+   * Sort by the upload time.
+   * {@param posts}: json array containing the post metadata
+   */
+   public sortNewest (posts) {
+      return posts.sort(function(lhs, rhs) {
+        return lhs.UploadDate - rhs.UploadDate;
+      });
+  }
+
+  /**
+   * Sort by the event time.
+   * {@param posts}: json array containing the post metadata
+   */
+  public sortEventTime(posts) {
+      return posts.sort(function(lhs, rhs) {
+        return lhs.EventDate - rhs.EventDate;
+      });
+  }
+
+}
