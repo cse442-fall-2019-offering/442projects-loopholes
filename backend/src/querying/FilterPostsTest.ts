632997f81d6db6404cdb0ee905fc5d9cdeda1bbd
Supratik Neupane <sneupane@buffalo.edu>
2019-11-12 14:08:22 -0500

    Added unit tests for filterIn to check a request with no matching posts #74

--- backend/src/querying/FilterPostsTest.ts
+++ backend/src/querying/FilterPostsTest.ts
@@ -2,7 +2,7 @@ import  FilterPosts from "./FilterPosts";
 import { expect } from "chai";
 import "mocha";
 
-// tests if there is a connection to the firebase database
+// tests that matching posts are returned
 describe("Test FilterIn", () => {
   it("Only the post that have metadata containing the search string should be included", async () => {
     const testPosts = [{"category":"meme", "UploadDate":5},
@@ -12,6 +12,18 @@ describe("Test FilterIn", () => {
 
     const filter = new FilterPosts();
     let actualPosts = filter.filterIn(testPosts, "meme");
+    expect(expectedPosts).deep.equal(actualPosts);
+  });
+});
+
+// tests that if no posts match, then an empty array is returned
+describe("Test FilterIn no matches", () => {
+  it("No matches from the database should result in an empty array", async () => {
+    const testPosts = [{"category":"meme", "UploadDate":5},
+      {"category":"not me-me", "UploadDate":15}, {"category":"memes", "UploadDate":50}];
+    const expectedPosts =  [];
+    const filter = new FilterPosts();
+    let actualPosts = filter.filterIn(testPosts, "posters");
     expect(expectedPosts).deep.equal(actualPosts);
   });
 });
