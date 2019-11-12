c2df275ae598fcc9bfa07934ebe1335861d20829
Supratik Neupane <sneupane@buffalo.edu>
2019-11-11 22:42:43 -0500

    Revert "Implemented sorting by event date in the backend #32"
    This reverts commit b714a36ef765b0d8bb6bc3062a75aac096fe0b3a.

--- backend/src/querying/SortPosts.ts
+++ backend/src/querying/SortPosts.ts
@@ -18,9 +18,10 @@ export default class SortPosts {
    * Sort by the event time.
    * {@param posts}: json array containing the post metadata
    */
-  public sortEventDate(posts) {
+  public sortEventTime(posts) {
       return posts.sort(function(lhs, rhs) {
         return lhs.EventDate - rhs.EventDate;
       });
   }
+
 }
