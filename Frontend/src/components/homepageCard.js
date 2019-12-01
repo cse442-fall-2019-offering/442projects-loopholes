1765bd07de2d7f5b22a1c6be2720aff2bf5eaa54
edentron <cleverforesttrees@gmail.com>
2019-12-01 11:09:28 -0500

    Implemented basic upvote button functionality for posts #61
    Added an UpvoteButton class to homepageCard.js and added it to the homepageCard. Every card starts with 2 upvotes. The upvotes are displayed on the button. Clicking the button once increments the count and fills in the button with the color blue. Clicking the button again will set upvotes back to the original upvote count and return the button to a blue outline (not filled in). Not sure if the count will stay updated after refreshing the page, or after moving to a different page then returning

--- Frontend/src/components/homepageCard.js
+++ Frontend/src/components/homepageCard.js
@@ -1,66 +1,98 @@
-import React from 'react';
-import Card from 'react-bootstrap/Card';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import ZoomIn from './zoomin';
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import ZoomIn from "./zoomin";
+import Button from "react-bootstrap/Button";
+
+class UpvoteButton extends React.Component{
+    state={
+        origVotes : 2,
+        updateVotes : 2,
+        bgColor: 'outline-primary'
+    };
+
+    updateVotesHandler=(event)=> {
+        event.preventDefault();
+        event.stopPropagation();
+        if(this.state.updateVotes === this.state.origVotes){
+            this.setState({ 
+                updateVotes: this.state.updateVotes + 1,
+                bgColor: 'primary'
+            });
+            
+        }else{
+            this.setState({ 
+                updateVotes: this.state.origVotes,
+                bgColor: 'outline-primary'
+            });
+        }
+    };
+
+    render(){
+        return (
+            <Button
+            size="sm"
+            variant={this.state.bgColor}
+            onClick={e=>this.updateVotesHandler(e)}
+            >{("" + this.state.updateVotes + " upvotes")}</Button>
+        );
+    };
+}
 
 const HomepageCard = props => {
-    const {
-        imageLink,
-        title,
-        date,
-        time,
-        place,
-        zoomed
-    } = props;
-    return (
-        <Card border="primary" bg="light" text="dark">
-                <Nav>
-                    <Nav.Item>
-                        <Nav.Link href={imageLink} target="_blank">
-                            <Card.Img variant="top" src={imageLink} />
-                        </Nav.Link>
-                    </Nav.Item>
-                </Nav>
-                <Card.Body>
-                    <Card.Title>{title}
-                            <br></br>
-                            <br></br>
-                            <small>
-                                <div className="font-weight-bold">Date:</div> {date}
-                                <br></br>
-                                <br></br>
-                                <div className="font-weight-bold">Time:</div> {time}
-                                <br></br>
-                                <br></br>
-                                <div className="font-weight-bold">Place:</div> {place}
-                            </small>
-                    </Card.Title>
-                </Card.Body>
-            <Card.Footer>
-                <div className="text-center">
-                    {!zoomed &&
-                    <ZoomIn cardProps={props}/>
-                    }
-                </div>
-                <div className="text-left">
-                    
-                </div>
-                <div className="text-right">
-                    <Form.Check text="dark" type="checkbox" label="Like" />
-                    //HANDLE CLICK
-                </div>
-            </Card.Footer>
-        </Card>    
-    );
+  const {
+    imageLink,
+    eventTitle,
+    eventDate,
+    eventTime,
+    eventLocation,
+    zoomed
+  } = props;
+  
+  return (
+    <Card border="primary" bg="light" text="dark">
+      <Nav>
+        <Nav.Item>
+          <Nav.Link href={imageLink} target="_blank">
+            <Card.Img variant="top" src={imageLink} />
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <Card.Body>
+        <Card.Title>
+          {eventTitle}
+          <br></br>
+          <br></br>
+          <small>
+            <div className="font-weight-bold">Date:</div> {eventDate}
+            <br></br>
+            <br></br>
+            <div className="font-weight-bold">Time:</div> {eventTime}
+            <br></br>
+            <br></br>
+            <div className="font-weight-bold">Place:</div> {eventLocation}
+          </small>
+        </Card.Title>
+      </Card.Body>
+      <Card.Footer>
+        <div className="text-center">
+          {!zoomed && <ZoomIn cardProps={props} />}
+        </div>
+        <div className="text-right">
+            <UpvoteButton></UpvoteButton>
+        </div>
+      </Card.Footer>
+    </Card>
+  );
 };
 
 HomepageCard.defaultProps = {
-    title: "Untitled",
-    date: "N/A",
-    time: "N/A",
-    place: "N/A",
-    zoomed: false
-}
+  title: "Untitled",
+  date: "N/A",
+  time: "N/A",
+  place: "N/A",
+  zoomed: false
+};
 
 export default HomepageCard;
